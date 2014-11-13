f1fb117834acc8b9b95c145abdcd03bf2c99778f
Matt Lambdin <lambdin.matthew@gmail.com>
2014-11-12 22:23:17 -0800

    Create hello_world.cpp
    Commit for hello-world tutorial repository!
    
    Simple C++ program. Includes main() function to print "Hello World!" to the screen.

---
+++ hello_world.cpp
@@ -0,0 +1,13 @@
+// hello-world.cpp
+// - prints "Hello World!" to the screen
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main ()
+{
+	cout << "Hello World!" << endl;
+	
+	return 0;
+}
